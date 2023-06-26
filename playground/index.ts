b1a18901d5be287368afa61e8c72e792eae41e93
bbcvc <1103787680@qq.com>
2023-06-26 16:10:26 +0800

    chore: add playground

---
+++ playground/index.ts
@@ -0,0 +1,11 @@
+function main() {
+  const array = ['a', 'b', 'c']
+  for (let index = 0; index < array.length; index++) {
+    let element = array[index]
+    if (element) {
+      element = 'aa'
+    }
+  }
+}
+
+main()
